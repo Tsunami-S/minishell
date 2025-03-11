7c9269aa109a03813dcd3f1abe77077c8774fbea
Tsunami-S <jvlipnitsk@gmail.com>
2025-03-11 18:51:35 +0900

    [3/11 18:50] fix: tokenizer.c クォーテーションに囲まれたリテラルの中に空白があったらそこで区切られてしまっていたミスの修正 [Before] start'hello world'end     [0]start'hello     [1]world'end [After] start'hello world'end     [0]start'hello world'end remain: クォーテーションで囲まれた文字列の処理

--- tokenizer.c
+++ tokenizer.c
@@ -6,13 +6,13 @@
 /*   By: tssaito <tssaito@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 20:09:16 by tssaito           #+#    #+#             */
-/*   Updated: 2025/03/10 00:04:45 by tssaito          ###   ########.fr       */
+/*   Updated: 2025/03/11 18:49:33 by tssaito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	isdigit_words(char *str, int len)
+static int	isdigit_words(const char *str, int len)
 {
 	int	i;
 
@@ -58,12 +58,39 @@ static void	make_new_token(char *str, int len, t_tokens **tokens)
 	}
 }
 
+static char	*get_token_size(const char *start, char *str)
+{
+	char	ope;
+
+	while (*str)
+	{
+		if (ft_isspace(*str))
+			break ;
+		if (isredirect(*str))
+		{
+			if (isdigit_words(start, str - start))
+				while (*str && isredirect(*str))
+					str++;
+			break ;
+		}
+		else if (*str == '\'' || *str == '\"')
+		{
+			ope = *str++;
+			while (*str && *str != ope && !isredirect(*str))
+				str++;
+			if (*str == ope)
+				str++;
+		}
+		else
+			str++;
+	}
+	return (str);
+}
+
 t_tokens	*tokenizer(char *str)
 {
 	t_tokens	*tokens;
 	char		*start;
-	int			si;
-	int			ei;
 
 	tokens = NULL;
 	while (*str)
@@ -73,11 +100,7 @@ t_tokens	*tokenizer(char *str)
 		if (!*str)
 			break ;
 		start = str;
-		while (*str && !ft_isspace(*str) && !isredirect(*str))
-			str++;
-		if (*str && isredirect(*str) && isdigit_words(start, str - start))
-			while (*str && isredirect(*str))
-				str++;
+		str = get_token_size(start, start);
 		make_new_token(start, str - start, &tokens);
 	}
 	return (tokens);
