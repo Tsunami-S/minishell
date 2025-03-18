bf44858d64180ab9f9a0d9e4fed24d7ee6dc792e
Tsunami-S <jvlipnitsk@gmail.com>
2025-03-19 00:18:37 +0900

    [03/19 00:18] tokensの先頭がnull文字のときのセグフォ修正

--- expander/expander.c
+++ expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: tssaito <tssaito@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 14:21:12 by tssaito           #+#    #+#             */
-/*   Updated: 2025/03/18 21:23:57 by tssaito          ###   ########.fr       */
+/*   Updated: 2025/03/19 00:17:29 by tssaito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 t_tokens	*expander(char *str, t_var **varlist)
 {
 	t_tokens	*tokens;
+	t_tokens	*tmp;
 
 	tokens = NULL;
 	tokens = pre_tokenizer(str);
@@ -24,6 +25,13 @@ t_tokens	*expander(char *str, t_var **varlist)
 	{
 		free_tokens(&tokens);
 		return (NULL);
+	}
+	while (!tokens->token[0])
+	{
+		tmp = tokens;
+		tokens = tokens->next;
+		free(tmp->token);
+		free(tmp);
 	}
 	more_tokenizer(&tokens);
 	return (tokens);
